fce5aa9d62db9f42e870fac99ffa21eb80015d90
Dorian Gomes <dorian@Dorian.localdomain>
2022-10-26 19:10:36 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import express from 'express'
+
+const APP_PORT = 1234
+
+const app = express()
+// app.use('/pets', PetsController)
+app.get('/', (req, res) => res.send('🏠'))
+app.listen(APP_PORT, () => console.log(`Silence, ça tourne sur le port ${APP_PORT}.`))
