6a9fd3d0f21edb736708d5e484869e3cbe76f36a
Dorian Gomes <dorian@Dorian.localdomain>
2022-10-26 19:17:50 +0200

    added basic git ignore

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,6 @@ const APP_PORT = 1234
 const app = express()
 // app.use('/pets', PetsController)
 app.get('/', (req, res) => res.send('🏠'))
+
+
 app.listen(APP_PORT, () => console.log(`Silence, ça tourne sur le port ${APP_PORT}.`))
